dca9ae8a544d325676ca7c3cd0a55c0cf0321db9
Riume <riumecarlos@hotmail.com>
2024-10-12 13:30:18 -0300

    feat(project): commit inicial

---
+++ src/main/java/br/com/riume/backendeureka/BackendEurekaApplication.java
@@ -0,0 +1,13 @@
+package br.com.riume.backendeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/riume/backendeureka/BackendEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.riume.backendeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
